f8733ba21daad3810cda560c86949692ab775973
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2024-03-06 07:33:39 +0530

    Add files via upload

---
+++ js-101/session1/session1.js
@@ -0,0 +1,57 @@
+// console.log("Hello world");
+// console.log(2);
+// console.log("Kuldeep Yadav")
+
+////////////--- 1. var keyword ---/////
+
+// var a; // declaration
+// a=5; // assignment
+
+// var a = 5 // declaration + assignment = initialization
+
+// var b = 10;
+// console.log(b)
+
+// // b = 15; // re-assignment is allowed in var
+// // console.log(b)
+
+// var b = 20; // rer-declaration is allowed in var
+// console.log(b)
+
+// var greeter;
+// greeter = "hey hi";
+// console.log(greeter);
+// greeter = "Hey";
+// console.log(greeter)
+
+////////////--- 2. const keyword ---///////
+
+// const weekDays = 7;
+// console.log(weekDays);
+
+// weekDays = 10; // re-assignment is not allowed in const
+// console.log(weekDays)
+
+// const weekDays = 10; // rer-declaration is not allowed in const
+// console.log(weekDays)
+
+
+////////////--- 3. let keyword ---///////
+
+// let a = 11;
+// console.log(a);
+
+// // let a =15; // re-declaration is not allowed in let
+// // console.log(a);
+
+a = 10; // re-assignment is allowed in let
+console.log(a)
+
+///------Valid variable name------///
+
+// let x = 10;
+// let marks= 10;
+
+// let date_of_birth = "12-12-1995"
+// let dateOfBirth = "12-12-1995" // camel case
+
