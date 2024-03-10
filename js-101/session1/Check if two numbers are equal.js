4e2e5caf04b5a0a930fd55ad74108b070b4b0f3f
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2024-03-10 20:10:21 +0530

    Add files via upload

---
+++ js-101/session1/Check if two numbers are equal.js
@@ -0,0 +1,17 @@
+function isEqual(num1, num2) {
+    if (num1 === num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+if (isEqual(3, 3) !== true)
+
+    console.log("Test fails: Expected true for input a = 3 and b = 3");
+
+else
+
+    console.log("Sample test case for input a = 3 and b = 3 passed!");
